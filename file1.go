b21ab68bec034ff99d90083129fb0e4c408bdc15
klecxe <136313164+klecxe@users.noreply.github.com>
2026-03-15 14:21:55 +0300

    data types

--- file1.go
+++ file1.go
@@ -1,13 +1,9 @@
 package main
 import "fmt"
 func main() {
-    var a, b, c int
-    fmt.Scan(&a)
-    fmt.Scan(&b)
-    fmt.Scan(&c)
-    for i := 1 ; i <= a ; i++ {
-        if i % b == 0 && i % c != 0 {
-        fmt.Print(i)
-        }
-    }
+    var isAlive bool = true
+    var isEnabled bool = false
+ 
+    fmt.Println("isAlive: ", isAlive)
+    fmt.Println("isEnabled: ", isEnabled)
 }
