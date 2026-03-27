1540daca71e3f412b8d08bb2a21ecb9e35294a45
klecxe <136313164+klecxe@users.noreply.github.com>
2026-03-28 00:55:43 +0400

    merge

--- file1.go
+++ file1.go
@@ -1,9 +1,15 @@
 package main
 import "fmt"
-func main() {
-    var isAlive bool = true
-    var isEnabled bool = false
- 
-    fmt.Println("isAlive: ", isAlive)
-    fmt.Println("isEnabled: ", isEnabled)
+func main () {
+    arr := [100]int {}
+    var a int
+    fmt.Scan(&a)
+   for i := 0; i < a; i++{
+        fmt.Scan(&arr[i])
+               }
+       for inx,elem := range arr{
+           if inx % 2 == 0 && elem != 0 {
+           fmt.Printf("%d ", elem)
+           }
+   }
 }
